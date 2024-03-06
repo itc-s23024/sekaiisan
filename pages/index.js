573dd38e71f7c5c17e7418a585c002e3436f54ea
又吉陸宇 <s23024@std.it-college.ac.jp>
2024-03-06 06:10:46 +0000

    星座占い 著作権直した 3/6(水)

--- pages/index.js
+++ pages/index.js
@@ -1,49 +1,22 @@
-import React, { useState } from 'react'
-
 const Seiza = ({ content }) => {
-  // ランキングの状態を管理するuseStateフック
-  const [ranking, setRanking] = useState([])
-
-  // ランダムなスコアを生成する関数
-  const generateRandomScore = () => {
-    // 0から100の間のランダムな整数を生成する
-    return Math.floor(Math.random() * 101)
-  }
-
-  // ランキングを生成する関数
-  const generateRanking = () => {
-    // 星座ごとのランダムなスコアを計算する
-    const horoscopesWithScores = content.horoscope['2024/03/content'].map(
-      horoscope => ({
-        ...horoscope,
-        score: generateRandomScore()
-      })
-    )
-
-    // スコアに基づいてランキングをソートする
-    const sortedRanking = horoscopesWithScores.sort((a, b) => b.score - a.score)
-
-    // ランキングを更新する
-    setRanking(sortedRanking)
-  }
-
-  // 初回のみランキングを生成する
-  useState(() => {
-    generateRanking()
-  }, [])
-
   return (
     <>
       <h1>本日の星座占い</h1>
-      {ranking.map((horoscope, index) => (
-        <div key={horoscope.sign}>
-          <p>
-            {index + 1}位: {horoscope.sign}
-          </p>
+      {content.horoscope['2024/03/content'].map((horoscope, index) => (
+        <div key={index}>
+          <p>{horoscope.rank}位</p>
+          <p>{horoscope.sign}</p>
           <p>本日のラッキーアイテムは{horoscope.item}</p>
           <p>{horoscope.content}</p>
         </div>
       ))}
+      <p>利用したAPIはこちら</p>
+      <p>
+        powerd by <a href='http://jugemkey.jp/api/'>JugemKey</a>
+      </p>
+      <p>
+        【PR】<a href='http://www.tarim.co.jp/'>原宿占い館 塔里木</a>
+      </p>
     </>
   )
 }
